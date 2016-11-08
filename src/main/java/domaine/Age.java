1f329c86bc8a091c5863e0831fc4c99546aaa812
Jean-Philippe LETARD <jletard.prestataire@voyages-sncf.com>
2016-11-08 16:35:17 +0100

    Ajout des points de confilt

--- src/main/java/domaine/Age.java
+++ src/main/java/domaine/Age.java
@@ -4,14 +4,17 @@ package domaine;
  * Created by jean_letard on 18/10/2016.
  */
 public enum Age {
-    I(SensRotation.GAUCHE),
-    II(SensRotation.DROITE),
-    III(SensRotation.GAUCHE);
+    I(SensRotation.GAUCHE, 1),
+    II(SensRotation.DROITE,3),
+    III(SensRotation.GAUCHE,5);
 
     final SensRotation sensRotation;
+    final int nbPointDefaiteMilitaire = -1;
+    final int nbPointVictoireMilitaire;
 
-    Age(SensRotation sensRotation) {
+    Age(SensRotation sensRotation, int nbPointVictoireMilitaire) {
        this.sensRotation = sensRotation;
+        this.nbPointVictoireMilitaire = nbPointVictoireMilitaire;
     }
 
     public enum SensRotation {GAUCHE, DROITE}
